82c6991bea237fa0c714fcafc1e40fa4549b3ec0
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2022-10-01 18:38:25 -0400

    got a white rectangle in the corner!

--- game/src/game/GamePanel.java
+++ game/src/game/GamePanel.java
@@ -2,6 +2,8 @@ package game;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -35,8 +37,19 @@ public class GamePanel extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		while (gameThread != null) {
-			System.out.println("The game loop!");
+			update();
+			repaint();
 		}
 	}
+	
+	public void update() {
+		
+	}
 
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setColor(Color.white);
+		g2.fillRect(0, 0, TILE_SIZE, TILE_SIZE);
+	}
 }
