609d703fa0e6c13096e7828a36ef7d388565983d
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2022-10-01 22:55:57 -0400

    got a background reader from a map txt file.

--- game/src/tile/TileManager.java
+++ game/src/tile/TileManager.java
@@ -1,7 +1,10 @@
 package tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -11,11 +14,14 @@ public class TileManager {
 
 	GamePanel gamePanel;
 	Tile[] tile;
+	int mapTileNumber[][];
 	
 	public TileManager(GamePanel gp) {
 		gamePanel = gp;
 		tile = new Tile[10];
+		mapTileNumber = new int[gamePanel.MAX_SCREEN_COLUMNS][gamePanel.MAX_SCREEN_ROWS];
 		getTileImage();
+		loadMap();
 	}
 	
 	public void getTileImage() {
@@ -31,6 +37,33 @@ public class TileManager {
 			e.printStackTrace();
 		}
 	}
+	
+	public void loadMap() {
+		try {
+			InputStream inputStream = getClass().getResourceAsStream("/maps/map.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+			int column = 0;
+			int row = 0;
+			while (column < gamePanel.MAX_SCREEN_COLUMNS && row < gamePanel.MAX_SCREEN_ROWS) {
+				String line = br.readLine();
+				while (column < gamePanel.MAX_SCREEN_COLUMNS) {
+					String numbers[] = line.split(" ");
+					int number = Integer.parseInt(numbers[column]);
+					mapTileNumber[column][row] = number;
+					column++;
+				}
+				if (column == gamePanel.MAX_SCREEN_COLUMNS) {
+					column = 0;
+					row++;
+				}
+			}
+			br.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void draw(Graphics2D g2) {
 		int column = 0;
 		int row = 0;
@@ -39,7 +72,8 @@ public class TileManager {
 		
 		while (column < gamePanel.MAX_SCREEN_COLUMNS &&
 			   row < gamePanel.MAX_SCREEN_ROWS) {
-			g2.drawImage(tile[0].image, x, y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
+			int tileNumber = mapTileNumber[column][row];
+			g2.drawImage(this.tile[tileNumber].image, x, y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
 			column++;
 			x += gamePanel.TILE_SIZE;
 			
