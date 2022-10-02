441a2f132c5708a92ee83aa2f42b186af0955651
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2022-10-01 21:12:12 -0400

    got this cursed mudkip drawn w/o animation

--- game/src/entity/Player.java
+++ game/src/entity/Player.java
@@ -2,6 +2,10 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import game.GamePanel;
 import game.KeyHandler;
@@ -14,32 +18,67 @@ public class Player extends Entity{
 		this.gamePanel = gp;
 		this.keyHandler = kh;
 		setDefaultValues();
+		getPlayerImage();
 	}
 	
 	public void setDefaultValues() {
 		this.x = 100;
 		this.y = 100;
 		this.speed = 4;
+		this.direction = "down";
+	}
+	
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipBack1.png"));
+			up2 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipBack2.png"));
+			down1 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipFront1.png"));
+			down2 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipFront2.png"));
+			left1 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipLeft1.png"));
+			left2 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipLeft2.png"));
+			right1 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipRight1.png"));
+			right2 = ImageIO.read(getClass().getResourceAsStream("/player/mudkipRight2.png"));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void update() {
 		if (keyHandler.upPressed == true) {
+			direction = "up";
 			this.y -= this.speed;
 		}
 		if (keyHandler.downPressed == true) {
+			direction = "down";
 			this.y += this.speed;
 		}
 		if (keyHandler.leftPressed == true) {
+			direction = "left";
 			this.x -= this.speed;
 		}
 		if (keyHandler.rightPressed == true) {
+			direction = "right";
 			this.x += this.speed;
 		}
 	}
 	
 	public void draw(Graphics2D g2) {
-		g2.setColor(Color.white);
-		g2.fillRect(this.x, this.y,
-					gamePanel.TILE_SIZE, gamePanel.TILE_SIZE);
+		BufferedImage image = null;
+		switch(direction) {
+			case "up":
+				image = up1;
+				break;
+			case "down":
+				image = down1;
+				break;
+			case "right":
+				image = right1;
+				break;
+			case "left":
+				image = left1;
+				break;
+		}
+		g2.drawImage(image, this.x, this.y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
 	}
 }
