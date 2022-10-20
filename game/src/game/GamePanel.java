5d2f0e049f6db8304ffbe704b9d7920ca34242d1
NicholasQNguyen <NicholasQNguyen8@gmail.com>
2022-10-20 12:12:54 -0400

    cleanup

--- game/src/game/GamePanel.java
+++ game/src/game/GamePanel.java
@@ -47,14 +47,6 @@ public class GamePanel extends AbstractPanel implements Runnable {
   // List to hold the enemies in the game
   private List<Entity> enemyList = new CopyOnWriteArrayList<Entity>();
  
-  // Popup window for victory.
-  JPanel mudkipP;
-  JPanel goblinP;
-  PopupFactory pf;
-
-  private Popup mudkipPop;
-  private Popup goblinPop;
-
   /** Constructor.
    *
    */
@@ -62,18 +54,7 @@ public class GamePanel extends AbstractPanel implements Runnable {
     super();
     this.addKeyListener(playerKeyHandler);
     this.addKeyListener(goblinKeyHandler);
-    mudkipP = new JPanel();
-    goblinP = new JPanel();
     
-    JLabel mudkipWin = new JLabel("MUDKIP WINS");
-    JLabel goblinWin = new JLabel("GOBLIN WINS");
-
-    mudkipP.add(mudkipWin);
-    goblinP.add(goblinWin);
-    
-    pf = new PopupFactory();
-    mudkipPop = pf.getPopup(this, mudkipP, 180, 100);
-    goblinPop = pf.getPopup(this, goblinP, 180, 100);
     // TODO Get controllers working.
     // this.jsHandler = new JoystickHandler();
 
